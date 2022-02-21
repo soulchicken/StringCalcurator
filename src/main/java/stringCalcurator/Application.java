ec62adad710923b23fa756df88c2a101b93793a3
소울치킨 <soulfever01@naver.com>
2022-02-21 15:14:08 +0900

    FEAT : scanner를 통해 문자열을 input

--- src/main/java/stringCalcurator/Application.java
+++ src/main/java/stringCalcurator/Application.java
@@ -1,10 +1,13 @@
 package stringCalcurator;
 
+import java.util.Scanner;
+
 public class Application {
     public static int answer = 0;
 
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
         System.out.println(answer);
     }
 }
