ce341dabbd0f9d27b6471e5106ab426cf40af47b
소울치킨 <soulfever01@naver.com>
2022-02-21 15:24:27 +0900

    FEAT : null, ""이 입력됐는지 판별

--- src/main/java/stringCalcurator/Application.java
+++ src/main/java/stringCalcurator/Application.java
@@ -8,6 +8,17 @@ public class Application {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
+        if (inputNull(input)) {
+            // null이 아니면 이 안에서 여러 과정이 수행될 예정
+        }
         System.out.println(answer);
     }
+
+    public static boolean inputNull(String str) {
+        if (str.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+
 }
